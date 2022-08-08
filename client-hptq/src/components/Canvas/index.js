2283be08209e0ee609b853a7ae2f2be63345200a
“iamthechanger” <taroschenker@hotmail.co.uk>
2022-08-08 16:57:44 +0100

    combined the sockio code into canvas

--- client-hptq/src/components/Canvas/index.js
+++ client-hptq/src/components/Canvas/index.js
@@ -1,16 +1,28 @@
-import React, { useEffect, useState, useRef } from "react";
-import io from "socket.io-client";
-import { Container, Box, Text } from "@chakra-ui/react";
-import Word from "../Word";
+/*
+THE BELOW CODE WILL LET YOU TRACE WHAT ANOTHER USER IS DRAWING HOWEVER IT WILL NOT BE SMOOTH, THE SOLUTION IS TO DRAW WHAT WAS DRAWN AFTER THEY HAVE FINISHED.
+*/
 
-const Canvas = () => {
-  //   const [message, setMessage] = useState("");
-  //   const [messageReceived, setMessageReceived] = useState("");
-  const socket = io.connect("https://hptq-backend.herokuapp.com/");
-  const [userID, setUserID] = useState("");
+// import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState, useRef } from "react";
+// const socket = io.connect("http://localhost:3001");
+const socket = io.connect('https://hptq-backend.herokuapp.com/')
+
+function Canvas() {
+  /// socket
+  //message is for the messsage box at the top of the page, to show how sockets can send message to another page
+  const [message, setMessage] = useState({
+    guess: ''
+  });
+  const [messageReceived, setMessageReceived] = useState("");
+  const [messagelist, setMessagelist] = useState([]);
+  const [correctAnswer, setcorrectAnswer] = useState("carrot");
+  //not currently in use
+
+  const [userID, setUserID] = useState();
   const [userGameState, setUserGameState] = useState({
     username: userID,
-    isTurn: false,
+    isTurn: false
   });
 
   const [room, setRoom] = useState("");
@@ -20,21 +32,38 @@ const Canvas = () => {
   //canvas - sets state of canvas. useRef stores state without triggering a re-render
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
-
   //socket to send message. message goes to server and then it returned to everyone apart from sender using 'broadcast'
-  //   const sendMessage = () => {
-  //     socket.emit("send_message", { message });
-  //   };
+  const sendMessage = () => {
+      // setMessagelist((list)=> ['i am the test message'])
+       socket.emit("send_message", message, room);
+       setMessagelist(oldArray => [...oldArray, message.guess])
+      
+  };
 
   //se effect triggered when the socket value changes. socket is connected to the server listening for data
+
   useEffect(() => {
-    // socket.on("recieved_message", (data) => {
-    //   setMessageReceived(data.message);
-    // });
+    socket.on("recieved_message", (data) => {
+      setMessageReceived(data);
+      setMessagelist(oldArray => [...oldArray, data.guess])
+      if(data.guess === correctAnswer) {
+        console.log('acrtivate fireworks')
+      }
+       console.log('messagelist - ',messagelist)
+    });
     socket.on("recieved_canvas", newDrawing);
     socket.on("refreshed_canvas", refreshCanvas);
     socket.on("recieved_id", (data) => {
       setUserID(data);
+    });
+
+    socket.on("make_all_other_turns_false", (id) => {
+      setUserGameState({ isTurn: false });
+      console.log(
+        "make all other turns false user state",
+        userGameState.isTurn,
+        "and id is " + id
+      );
     });
   }, [socket]);
 
@@ -65,6 +94,7 @@ const Canvas = () => {
     setUserGameState({
       isTurn: !userGameState.isTurn,
     });
+    socket.emit("set_all_other_turns_false", room);
     refreshCanvas();
   };
 
@@ -72,7 +102,6 @@ const Canvas = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
-
     let base64ImageData = canvas.toDataURL("image/png");
     socket.emit("refresh_canvas", base64ImageData, room);
   };
@@ -83,7 +112,7 @@ const Canvas = () => {
     }
   };
   const startDrawing = ({ nativeEvent }) => {
-    console.log(userGameState.isTurn);
+    console.log("startdrawing user state", userGameState.isTurn);
     if (userGameState.isTurn) {
       const { offsetX, offsetY } = nativeEvent;
       contextRef.current.beginPath();
@@ -102,40 +131,36 @@ const Canvas = () => {
   };
 
   const draw = ({ nativeEvent }) => {
-    // console.log('draw function', isDrawing)
     if (!isDrawing) {
       return;
     }
     const { offsetX, offsetY } = nativeEvent;
     contextRef.current.lineTo(offsetX, offsetY);
     contextRef.current.stroke();
-    // socket.emit("send_canvas", {x: offsetX, y: offsetY });
   };
 
   return (
     <div className="App">
-      {/* <input onChange={(e) => setMessage(e.target.value)} /> */}
-      {/* <button onClick={sendMessage}> send message</button> */}
-      {/* <h1> Message </h1> */}
+      <input onChange={(e) => setMessage({guess: e.target.value})} />
+      <button onClick={sendMessage}> send message</button>
+      <h1> Message </h1>
       {/* {messageReceived} */}
+     { messagelist.map(i => i)}
 
-      <Container display="flex" flexDirection="column" p="10">
-        <Box>
-          <canvas
-            onMouseDown={startDrawing}
-            onMouseUp={finishDrawing}
-            onMouseMove={draw}
-            ref={canvasRef}
-            height={"500px"}
-            width={"500px"}
-            style={{ border: "1px solid black" }}
-          />
-        </Box>
-
-        <Word />
-        {/* <button onClick={handleSetMyTurn}>handleSetMyTurn</button> */}
-      </Container>
-      {/* <input
+      <div className="App">
+        <canvas
+          onMouseDown={startDrawing}
+          onMouseUp={finishDrawing}
+          onMouseMove={draw}
+          ref={canvasRef}
+          height={250}
+          width={250}
+          style={{ border: "1px solid black" }}
+        />
+        <button onClick={handleSetMyTurn}>handleSetMyTurn</button>
+        turn status: {userGameState.isTurn}
+      </div>
+      <input
         type="text"
         onChange={(e) => setRoom(e.target.value)}
         placeholder="room"
@@ -145,9 +170,9 @@ const Canvas = () => {
         placeholder="username"
         onChange={(e) => setUserName(e.target.value)}
       />
-      <button onClick={handleRoomSelect}> Goto room </button> */}
+      <button onClick={handleRoomSelect}> Goto room </button>
     </div>
   );
-};
+}
 
 export default Canvas;
