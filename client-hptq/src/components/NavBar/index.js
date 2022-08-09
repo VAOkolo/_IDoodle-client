d8ea01ec9ea4058f0ff5c24b70b134ff3830641a
Florencia Pezcara <fpezcara@gmail.com>
2022-08-09 09:47:11 +0100

    some changes

---
+++ client-hptq/src/components/NavBar/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Flex, Heading} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { AnimatedLink } from "../../animations/animatedLink";
+
+const NavBar = () => {
+  return (
+    <Flex
+      as={motion.nav}
+      bgImage="linear-gradient( 135deg, #ABDCFF 10%, #0396FF 100%);"
+      minH="10vh"
+      p={3}
+      boxShadow="xl"
+      justifyContent="space-between"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition="1.5s ease-in"
+      _hover={{ transform: "scale(1)", filter: "brightness(105%)" }}
+    >
+      <Heading
+        as={motion.h1}
+        fontSize="4xl"
+        bgImage="linear-gradient(60deg, #E21143, #FFB03A)"
+        // color="#fff"
+        bgClip="text"
+        // color="transparent"
+        letterSpacing={2}
+        initial={{ opacity: 0, y: -220 }}
+        animate={{ opacity: 1, y: 0, fontSize: "40px" }}
+        transition="1.5s ease-in"
+        p={3}
+      >
+        Scribble Do
+      </Heading>
+      <Flex
+        as="ul"
+        alignItems="center"
+        justifyContent="space-between"
+        minW="30%"
+      >
+        <AnimatedLink>
+          <NavLink to="/home">Home</NavLink>
+          {/*this should lead to somewhere else, preferrably the lobby or sth */}
+        </AnimatedLink>
+        <AnimatedLink>
+          <NavLink to="/lobby">New Game</NavLink>
+        </AnimatedLink>
+        <AnimatedLink>
+          <NavLink to="/leaderboard">Leader Board</NavLink>
+        </AnimatedLink>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default NavBar;
