088d81fda18b18ef43787e50bd9cb82d5080753f
Florencia Pezcara <fpezcara@gmail.com>
2022-08-09 16:47:29 +0100

    now lobby form looks better

--- client-hptq/src/components/Settings/index.js
+++ client-hptq/src/components/Settings/index.js
@@ -8,6 +8,7 @@ import {
   Select,
   Text,
 } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 export default function Settings() {
   const [categories, setCategories] = useState();
@@ -52,67 +53,108 @@ export default function Settings() {
   return (
     <Container
       display="flex"
-      h="80vh"
+      h="95vh"
       minH="100%"
       justifyContent="center"
       alignItems="center"
     >
       <FormControl
-        p={4}
+        as={motion.form}
+        p="10"
         display="flex"
         flexDirection="column"
-        alignContent="center"
-        justifyItems="center"
-        justifyContent="center"
+        justifyContent="space-between"
         alignItems="center"
         h="80%"
         w="70%"
-        boxShadow="rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px"
-
+        boxShadow="rgba(17, 17, 26, 0.1) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 48px"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition="1.2s ease"
+        borderRadius="1em"
       >
-        <FormLabel fontSize="xl" className="Title" alignSelf="start" p="2">
-          Settings
+        <FormLabel
+          fontSize="2xl"
+          className="Title"
+          p="2"
+          color="gray"
+          fontWeight="normal"
+        >
+          Choose your settings
         </FormLabel>
 
-        <Text as="label" htmlFor="rounds">
+        <Text alignSelf="start" as="label" htmlFor="rounds" fontSize="sm">
           Rounds
         </Text>
-        <Select onChange={updateInput} className="rounds-control" id="rounds">
+        <Select
+          onChange={updateInput}
+          className="rounds-control"
+          id="rounds"
+          cursor="pointer"
+        >
           {createOptions(1, 4, 1).map((option) => (
             <option>{option}</option>
           ))}
         </Select>
 
-        <Text as="label" htmlFor="seconds">
+        <Text alignSelf="start" as="label" htmlFor="seconds" fontSize="sm">
           Seconds
         </Text>
-        <Select onChange={updateInput} className="seconds-control" id="seconds">
+        <Select
+          onChange={updateInput}
+          className="seconds-control"
+          id="seconds"
+          cursor="pointer"
+        >
           {createOptions(30, 70, 10).map((option) => (
             <option>{option}</option>
           ))}
         </Select>
 
-        <Text as="label" htmlFor="difficulty">
+        <Text alignSelf="start" as="label" htmlFor="difficulty" fontSize="sm">
           Difficulty
         </Text>
         <Select
           onChange={updateInput}
           className="difficulty-control"
           id="difficulty"
+          cursor="pointer"
         >
           <option value="easy">Easy</option>
           <option value="medium">Medium</option>
           <option value="hard">Hard</option>
         </Select>
 
-        <Text as="label" htmlFor="difficulty">
+        <Text alignSelf="start" as="label" htmlFor="difficulty" fontSize="sm">
           Category
         </Text>
-        <Select onChange={updateInput} class="category-control" id="category">
+        <Select
+          onChange={updateInput}
+          class="category-control"
+          id="category"
+          cursor="pointer"
+        >
           {categories && categories.map((c) => <option>{c.name}</option>)}
         </Select>
 
-        <Button onClick={handleSubmit}>Start Game</Button>
+        <Button
+          mt={4}
+          onClick={handleSubmit}
+          letterSpacing="0.094em"
+          boxShadow="10px 10px 14px 1px rgb(0, 0, 0 / 20%)"
+          bg="#845ec2"
+          w="100%"
+          color="white"
+          fontWeight="normal"
+          whileHover={{
+            color: "#845ec2",
+            background: "white",
+            border: "#845ec2",
+            fontWeight: "bold",
+          }}
+        >
+          Start Game
+        </Button>
       </FormControl>
     </Container>
   );
