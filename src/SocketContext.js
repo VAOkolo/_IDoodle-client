99343c40c7d871c44c25baa4f885fc70716c5442
Ismael Ali <ismaelali78@outlook.com>
2022-08-11 12:34:06 +0100

    changed socket link to deployed server

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -1,9 +1,8 @@
 import React, { createContext, useState } from "react";
 import io from "socket.io-client";
 
-// const socket = io.connect("https://hptq-backend.herokuapp.com/");
-
-const socket = io.connect("http://localhost:4000");
+const socket = io.connect("https://hptq-backend.herokuapp.com/");
+// const socket = io.connect("http://localhost:4000");
 
 export const SocketContext = createContext();
 
