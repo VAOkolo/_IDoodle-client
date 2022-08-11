88f0cc81bfd21859505b17d01e0a473e57d2d1c7
“iamthechanger” <taroschenker@hotmail.co.uk>
2022-08-11 18:10:26 +0100

    styles

--- src/pages/Lobby/index.js
+++ src/pages/Lobby/index.js
@@ -17,6 +17,10 @@ import {
   TableCaption,
   TableContainer,
   Text,
+  VStack,
+  HStack,
+  StackDivider,
+  Box,
 } from "@chakra-ui/react";
 
 export default function Lobby() {
@@ -54,22 +58,50 @@ export default function Lobby() {
   }, []);
 
   return (
-    <div className="lobby">
-      <div className="lobbyPlayersContainer">
-        <h1>Players In Lobby</h1>
-        <div>
-          {availablePlayers.map((user, index) => (
-            <>
-              <Avatar name={user.username} src="https://bit.ly/broken-link">
-                <AvatarBadge bg="green.500" boxSize="1em" />
-              </Avatar>
-              <p>{user.username}</p>
-              <p>host: {user.host}</p>
-            </>
-          ))}
+    <>
+      <div className="lobby">
+        <Box      bg="linear-gradient( 135deg, #845EC2 10%, #D65DB1 100%)"     transition="0.5s linear"
+      initial={{ opacity: 0.55, x: 390 }}
+      animate={{ opacity: 1, x: 0 }}       justifyContent="center"
+      alignItems="center"
+       >
+          <div className="lobbyPlayersContainer">
+          <div>
+            <Text fontSize='3xl'> Players in Lobby</Text>
+            <Text fontSize='1xl'> Room Number: {room}</Text>
+            {availablePlayers.map((user, index) => (
+              <>
+                <VStack
+                  divider={<StackDivider borderColor="gray.200" />}
+                  spacing={4}
+                  align="stretch"
+                  justifyContent="center" 
+                  my={5} 
+                >
+                  <HStack spacing={5}>
+                    <Avatar
+                      name={user.username}
+                      src="https://bit.ly/broken-link"
+                    >
+                      <AvatarBadge bg="green.500" boxSize="1em" />
+                    </Avatar>
+                    {user.host ? (
+                      <Text  fontSize='1xl'> HOST: <span className="uppercase"> {user.username}</span></Text>
+                    ) : (
+                      <Text className="uppercase" fontSize='1xl'>{user.username}</Text>
+                    )}
+                  </HStack>
+            
+                </VStack>
+              </>
+            ))}
+          </div>
         </div>
+        
+        </Box>
+        
+        <Settings />
       </div>
-      <Settings />
-    </div>
+    </>
   );
 }
