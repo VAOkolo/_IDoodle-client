3f1dc4774a0e733407b3eac4c6eb8dca75d2bf52
Florencia Pezcara <fpezcara@gmail.com>
2022-08-12 09:45:40 +0100

    testing

---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,16 @@
+import { screen, render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Footer from ".";
+import { ThemeProvider } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+
+// export const ThemeWrapper = ({ children }) => (
+//   <ThemeProvider>{children}</ThemeProvider>
+
+describe("Footer", () => {
+  test("it renders a footer tag", () => {
+    render(<Footer />, { ThemeProvider });
+    const footer = screen.queryByRole("footer");
+    expect(footer).toBeInTheDocument();
+  });
+});
