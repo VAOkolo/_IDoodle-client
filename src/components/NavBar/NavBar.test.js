224c8bf959bb4acb91341b409b28158acfeb7914
VAOkolo <vincentaokolo@outlook.com>
2022-08-12 09:30:02 +0100

    testing branch

---
+++ src/components/NavBar/NavBar.test.js
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { NavBar } from './index'
+import { screen, render } from '@testing-library/react'
+
+describe("navbar present", () => {
+    test("The navbar is present", () => {
+        
+    })
+
+})
