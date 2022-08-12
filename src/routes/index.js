38b05c77ae3818b58b53ca79de554e7b24364221
Ismael Ali <ismaelali78@outlook.com>
2022-08-12 01:58:47 +0100

    added rules page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Home, Leaderboard, Lobby, GameRoom, GameOver } from "../pages";
+import { Home, Leaderboard, Lobby, GameRoom, GameOver, Rules } from "../pages";
 
 const AppRoutes = () => {
   return (
@@ -10,6 +10,7 @@ const AppRoutes = () => {
       <Route path="/leaderboard" element={<Leaderboard />} />
       <Route path="/lobby" element={<Lobby />} />
       <Route path="/game-over" element={<GameOver />} />
+      <Route path="/rules" element={<Rules />} />
     </Routes>
   );
 };
